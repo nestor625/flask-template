4a724376080573942b1d2b5b93231c109e3e2b69
nestor625 <nestor20010625@gmail.com>
2022-11-04 11:16:32 +0800

    01

--- template-flask-blueprint-with-factory/flask/app/view/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify, request, Blueprint, url_for, redirect, abort, make_response
-
-auth = Blueprint('autha', __name__)
-
-
-@auth.route('/')
-def index():
-    return 'welcome'
--- template-flask-i18n/flask/app/view/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for, Blueprint
-
-main = Blueprint('main', __name__)
-
-
-@main.route('/')
-def do_some_thing():
-    return render_template('index.html')
